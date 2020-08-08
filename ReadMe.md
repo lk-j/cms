b21a90627d72e34206290d69d109d2e413a691b1
lk-j <381584494@qq.com>
2020-08-08 19:30:08 +0800

    the initial edition

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+项目介绍：
+    发布技术文章的博客
+    主要模块
+        文章编辑后台
+            文章的增删改查
+            登录
+        前台显示
+            分页显示文章
+            
+    后台用mysql写入文章   MyBatis
+    前端从mysql读数据 用redis加速  用Elasticsearch采集日志 
+    用文档生产工具管理文档 Swagger-UI   
+    用docker部署项目
+    nacos+dubbo 实现服务化
+    Thymeleaf模板引擎渲染页面
+    
---
+++ admin/src/main/java/com/cms/admin/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.cms.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+@Controller
+@RequestMapping("/admin")
+public class LoginController {
+
+    /*@RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult login(@RequestBody UmsAdminLoginParam umsAdminLoginParam, BindingResult result) {
+        String token = adminService.login(umsAdminLoginParam.getUsername(), umsAdminLoginParam.getPassword());
+        if (token == null) {
+            return CommonResult.validateFailed("用户名或密码错误");
+        }
+        Map<String, String> tokenMap = new HashMap<>();
+        tokenMap.put("token", token);
+        tokenMap.put("tokenHead", tokenHead);
+        return CommonResult.success(tokenMap);
+    }*/
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String Login(HttpServletRequest request)
+    {
+        request.setAttribute("name", "login222");
+        return "login";
+    }
+}
